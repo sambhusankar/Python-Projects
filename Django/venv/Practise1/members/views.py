82a42968a4d959b72d1f25932ff7a56914303ae0
sambhusankar <sambhusankarswain@gmail.com>
2024-05-19 20:47:58 +0530

    django is in progress

--- Django/venv/Practise1/members/views.py
+++ Django/venv/Practise1/members/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
+    members =  Member.objects.all().values()
     template = loader.get_template('index.html')
+    context = {
+        "members" : members,
+    }
+    return HttpResponse(template.render(context,  request))
+def details(request):
+    template = loader.get_template("details.html")
     return HttpResponse(template.render())
